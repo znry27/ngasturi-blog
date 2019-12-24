adf3423a36fbfc02a891b3e48b34e6b435847256
znry27 <z.nry27@gmail.com>
2019-12-24 16:37:26 +0700

    Tutorial Action Server

--- tutorial_excel/static/js/custom.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$('.btn-delete-sale').click(function(){
-		var sale = $(this).data('sale-number');
-		alert('Are you sure want to delete Oder with number ' + sale);
-	});
-});
